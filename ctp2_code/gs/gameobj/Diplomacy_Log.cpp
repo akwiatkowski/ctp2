04640530163ea40fdc05fe38a97239c8dbab10f9
Aleksander Kwiatkowski <bobikx@poczta.fm>
2026-06-05 15:16:33 +0200

    refactor(phase-2): strcpy → strlcpy in Diplomacy_Log.cpp
    Converted 72 Case-A sites (dst is fixed-size array) using sizeof(dst).
    Skipped 21 Case-B/C sites (dst is char* or expression — unknown capacity)
    with TODO comments for a follow-up pass.

--- ctp2_code/gs/gameobj/Diplomacy_Log.cpp
+++ ctp2_code/gs/gameobj/Diplomacy_Log.cpp
@@ -15,7 +15,7 @@
 
 Diplomacy_Log::Diplomacy_Log()
 {
-    strcpy(m_filename,"logs\\diplomacy_log.txt");
+    strlcpy(m_filename, "logs\\diplomacy_log.txt", sizeof(m_filename));
     m_player_bit_mask = 0xffffffff;
     FILE *fout = fopen(m_filename, "w");
     Assert(fout);
@@ -162,34 +162,34 @@ void Diplomacy_Log::LogHostileAction(const UNIT_ORDER_TYPE order_type, const sin
 
     char order_str[80];
     switch(order_type) {
-    	case UNIT_ORDER_INVESTIGATE_CITY: strcpy (order_str, "UNIT_ORDER_INVESTIGATE_CITY"); break;
-        case UNIT_ORDER_NULLIFY_WALLS: strcpy (order_str, "UNIT_ORDER_NULLIFY_WALLS"); break;
-        case UNIT_ORDER_STEAL_TECHNOLOGY:strcpy (order_str, "UNIT_ORDER_STEAL_TECHNOLOGY"); break;
-        case UNIT_ORDER_ASSASSINATE: strcpy (order_str, "UNIT_ORDER_ASSASSINATE"); break;
-        case UNIT_ORDER_INVESTIGATE_READINESS:strcpy (order_str, "UNIT_ORDER_INVESTIGATE_READINESS"); break;
-        case UNIT_ORDER_FRANCHISE: strcpy (order_str, "UNIT_ORDER_FRANCHISE"); break;
-        case UNIT_ORDER_SUE_FRANCHISE: strcpy (order_str, "UNIT_ORDER_SUE_FRANCHISE"); break;
-        case UNIT_ORDER_CAUSE_UNHAPPINESS: strcpy (order_str, "UNIT_ORDER_CAUSE_UNHAPPINESS"); break;
-        case UNIT_ORDER_PLANT_NUKE: strcpy (order_str, "UNIT_ORDER_PLANT_NUKE"); break;
-        case UNIT_ORDER_SLAVE_RAID: strcpy (order_str, "UNIT_ORDER_SLAVE_RAID"); break;
-        case UNIT_ORDER_UNDERGROUND_RAILWAY: strcpy (order_str, "case UNIT_ORDER_UNDERGROUND_RAILWAY"); break;
-        case UNIT_ORDER_INCITE_UPRISING: strcpy (order_str, "UNIT_ORDER_INCITE_UPRISING"); break;
-        case UNIT_ORDER_BIO_INFECT: strcpy (order_str, "UNIT_ORDER_BIO_INFECT"); break;
-        case UNIT_ORDER_NANO_INFECT: strcpy (order_str, "UNIT_ORDER_NANO_INFECT"); break;
-        case UNIT_ORDER_CONVERT: strcpy (order_str, "UNIT_ORDER_CONVERT"); break;
-        case UNIT_ORDER_INDULGENCE: strcpy (order_str, "UNIT_ORDER_INDULGENCE"); break;
-        case UNIT_ORDER_SOOTHSAY: strcpy (order_str, "UNIT_ORDER_SOOTHSAY"); break;
-        case UNIT_ORDER_INJOIN: strcpy (order_str, "UNIT_ORDER_INJOIN"); break;
-        case UNIT_ORDER_INCITE_REVOLUTION: strcpy (order_str, "UNIT_ORDER_INCITE_REVOLUTION"); break;
-        case UNIT_ORDER_CREATE_PARK: strcpy (order_str, "UNIT_ORDER_CREATE_PARK"); break;
-        case UNIT_ORDER_BOMBARD: strcpy (order_str, "UNIT_ORDER_BOMBARD"); break;
-        case UNIT_ORDER_EXPEL: strcpy (order_str, "UNIT_ORDER_EXPEL"); break;
-        case UNIT_ORDER_ENSLAVE_SETTLER: strcpy (order_str, "UNIT_ORDER_ENSLAVE_SETTLER"); break;
-        case UNIT_ORDER_FINISH_ATTACK: strcpy (order_str, "UNIT_ORDER_FINISH_ATTACK"); break;
-        case UNIT_ORDER_SUE: strcpy (order_str, "UNIT_ORDER_SUE"); break;
-        case UNIT_ORDER_PILLAGE: strcpy (order_str, "UNIT_ORDER_PILLAGE"); break;
-        case UNIT_ORDER_INTERCEPT_TRADE: strcpy (order_str, "UNIT_ORDER_INTERCEPT_TRADE"); break;
-        case UNIT_ORDER_INFORM_AI_CAPTURE_CITY: strcpy (order_str, "UNIT_ORDER_INFORM_AI_CAPTURE_CITY"); break;
+    	case UNIT_ORDER_INVESTIGATE_CITY: strlcpy(order_str, "UNIT_ORDER_INVESTIGATE_CITY", sizeof(order_str)); break;
+        case UNIT_ORDER_NULLIFY_WALLS: strlcpy(order_str, "UNIT_ORDER_NULLIFY_WALLS", sizeof(order_str)); break;
+        case UNIT_ORDER_STEAL_TECHNOLOGY:strlcpy(order_str, "UNIT_ORDER_STEAL_TECHNOLOGY", sizeof(order_str)); break;
+        case UNIT_ORDER_ASSASSINATE: strlcpy(order_str, "UNIT_ORDER_ASSASSINATE", sizeof(order_str)); break;
+        case UNIT_ORDER_INVESTIGATE_READINESS:strlcpy(order_str, "UNIT_ORDER_INVESTIGATE_READINESS", sizeof(order_str)); break;
+        case UNIT_ORDER_FRANCHISE: strlcpy(order_str, "UNIT_ORDER_FRANCHISE", sizeof(order_str)); break;
+        case UNIT_ORDER_SUE_FRANCHISE: strlcpy(order_str, "UNIT_ORDER_SUE_FRANCHISE", sizeof(order_str)); break;
+        case UNIT_ORDER_CAUSE_UNHAPPINESS: strlcpy(order_str, "UNIT_ORDER_CAUSE_UNHAPPINESS", sizeof(order_str)); break;
+        case UNIT_ORDER_PLANT_NUKE: strlcpy(order_str, "UNIT_ORDER_PLANT_NUKE", sizeof(order_str)); break;
+        case UNIT_ORDER_SLAVE_RAID: strlcpy(order_str, "UNIT_ORDER_SLAVE_RAID", sizeof(order_str)); break;
+        case UNIT_ORDER_UNDERGROUND_RAILWAY: strlcpy(order_str, "case UNIT_ORDER_UNDERGROUND_RAILWAY", sizeof(order_str)); break;
+        case UNIT_ORDER_INCITE_UPRISING: strlcpy(order_str, "UNIT_ORDER_INCITE_UPRISING", sizeof(order_str)); break;
+        case UNIT_ORDER_BIO_INFECT: strlcpy(order_str, "UNIT_ORDER_BIO_INFECT", sizeof(order_str)); break;
+        case UNIT_ORDER_NANO_INFECT: strlcpy(order_str, "UNIT_ORDER_NANO_INFECT", sizeof(order_str)); break;
+        case UNIT_ORDER_CONVERT: strlcpy(order_str, "UNIT_ORDER_CONVERT", sizeof(order_str)); break;
+        case UNIT_ORDER_INDULGENCE: strlcpy(order_str, "UNIT_ORDER_INDULGENCE", sizeof(order_str)); break;
+        case UNIT_ORDER_SOOTHSAY: strlcpy(order_str, "UNIT_ORDER_SOOTHSAY", sizeof(order_str)); break;
+        case UNIT_ORDER_INJOIN: strlcpy(order_str, "UNIT_ORDER_INJOIN", sizeof(order_str)); break;
+        case UNIT_ORDER_INCITE_REVOLUTION: strlcpy(order_str, "UNIT_ORDER_INCITE_REVOLUTION", sizeof(order_str)); break;
+        case UNIT_ORDER_CREATE_PARK: strlcpy(order_str, "UNIT_ORDER_CREATE_PARK", sizeof(order_str)); break;
+        case UNIT_ORDER_BOMBARD: strlcpy(order_str, "UNIT_ORDER_BOMBARD", sizeof(order_str)); break;
+        case UNIT_ORDER_EXPEL: strlcpy(order_str, "UNIT_ORDER_EXPEL", sizeof(order_str)); break;
+        case UNIT_ORDER_ENSLAVE_SETTLER: strlcpy(order_str, "UNIT_ORDER_ENSLAVE_SETTLER", sizeof(order_str)); break;
+        case UNIT_ORDER_FINISH_ATTACK: strlcpy(order_str, "UNIT_ORDER_FINISH_ATTACK", sizeof(order_str)); break;
+        case UNIT_ORDER_SUE: strlcpy(order_str, "UNIT_ORDER_SUE", sizeof(order_str)); break;
+        case UNIT_ORDER_PILLAGE: strlcpy(order_str, "UNIT_ORDER_PILLAGE", sizeof(order_str)); break;
+        case UNIT_ORDER_INTERCEPT_TRADE: strlcpy(order_str, "UNIT_ORDER_INTERCEPT_TRADE", sizeof(order_str)); break;
+        case UNIT_ORDER_INFORM_AI_CAPTURE_CITY: strlcpy(order_str, "UNIT_ORDER_INFORM_AI_CAPTURE_CITY", sizeof(order_str)); break;
         default:
             { BOOL unknown_order_type=0;
                 Assert(unknown_order_type);
@@ -237,25 +237,25 @@ void Diplomacy_Log::LogMakeAgreement(const sint32 owner, const sint32 recipient,
 
     char astr[80];
     switch (agreement) {
-    case AGREEMENT_TYPE_DEMAND_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_DEMAND_ADVANCE"); break;
-	case AGREEMENT_TYPE_DEMAND_CITY: strcpy (astr, "AGREEMENT_TYPE_DEMAND_CITY");break;
-	case AGREEMENT_TYPE_DEMAND_MAP: strcpy (astr, "AGREEMENT_TYPE_DEMAND_MAP");break;
-	case AGREEMENT_TYPE_DEMAND_GOLD: strcpy (astr, "AGREEMENT_TYPE_DEMAND_GOLD");break;
-	case AGREEMENT_TYPE_DEMAND_STOP_TRADE: strcpy (astr, "AGREEMENT_TYPE_DEMAND_STOP_TRADE");break;
-	case AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS: strcpy (astr, "AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS"); break;
-    case AGREEMENT_TYPE_REDUCE_POLLUTION: strcpy (astr, "AGREEMENT_TYPE_REDUCE_POLLUTION"); break;
-	case AGREEMENT_TYPE_CEASE_FIRE: strcpy (astr, "AGREEMENT_TYPE_CEASE_FIRE");break;
-    case AGREEMENT_TYPE_PACT_CAPTURE_CITY: strcpy (astr, "AGREEMENT_TYPE_PACT_CAPTURE_CITY");break;
-    case AGREEMENT_TYPE_PACT_END_POLLUTION: strcpy (astr, "AGREEMENT_TYPE_PACT_END_POLLUTION");break;
-    case AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY: strcpy (astr, "AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY");break;
-	case AGREEMENT_TYPE_OFFER_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_OFFER_ADVANCE");break;
-	case AGREEMENT_TYPE_OFFER_CITY: strcpy (astr, "AGREEMENT_TYPE_OFFER_CITY");break;
-	case AGREEMENT_TYPE_OFFER_MAP: strcpy (astr, "AGREEMENT_TYPE_OFFER_MAP");break;
-	case AGREEMENT_TYPE_OFFER_GOLD: strcpy (astr, "AGREEMENT_TYPE_OFFER_GOLD");break;
-	case AGREEMENT_TYPE_EXCHANGE_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_ADVANCE");break;
-	case AGREEMENT_TYPE_EXCHANGE_CITY: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_CITY");break;
-	case AGREEMENT_TYPE_EXCHANGE_MAP: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_MAP");break;
-	case AGREEMENT_TYPE_NO_PIRACY: strcpy (astr, "AGREEMENT_TYPE_NO_PIRACY"); break;
+    case AGREEMENT_TYPE_DEMAND_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_ADVANCE", sizeof(astr)); break;
+	case AGREEMENT_TYPE_DEMAND_CITY: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_MAP: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_GOLD: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_GOLD", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_STOP_TRADE: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_STOP_TRADE", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS", sizeof(astr)); break;
+    case AGREEMENT_TYPE_REDUCE_POLLUTION: strlcpy(astr, "AGREEMENT_TYPE_REDUCE_POLLUTION", sizeof(astr)); break;
+	case AGREEMENT_TYPE_CEASE_FIRE: strlcpy(astr, "AGREEMENT_TYPE_CEASE_FIRE", sizeof(astr));break;
+    case AGREEMENT_TYPE_PACT_CAPTURE_CITY: strlcpy(astr, "AGREEMENT_TYPE_PACT_CAPTURE_CITY", sizeof(astr));break;
+    case AGREEMENT_TYPE_PACT_END_POLLUTION: strlcpy(astr, "AGREEMENT_TYPE_PACT_END_POLLUTION", sizeof(astr));break;
+    case AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_OFFER_ADVANCE", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_CITY: strlcpy(astr, "AGREEMENT_TYPE_OFFER_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_MAP: strlcpy(astr, "AGREEMENT_TYPE_OFFER_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_GOLD: strlcpy(astr, "AGREEMENT_TYPE_OFFER_GOLD", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_ADVANCE", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_CITY: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_MAP: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_NO_PIRACY: strlcpy(astr, "AGREEMENT_TYPE_NO_PIRACY", sizeof(astr)); break;
     default :
         {
         sint32 unknown_agreement_type=0;
@@ -284,25 +284,25 @@ void Diplomacy_Log::LogBrokenAgreement(const sint32 owner, const sint32 recipien
 
     char astr[80];
     switch (agreement) {
-    case AGREEMENT_TYPE_DEMAND_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_DEMAND_ADVANCE"); break;
-	case AGREEMENT_TYPE_DEMAND_CITY: strcpy (astr, "AGREEMENT_TYPE_DEMAND_CITY");break;
-	case AGREEMENT_TYPE_DEMAND_MAP: strcpy (astr, "AGREEMENT_TYPE_DEMAND_MAP");break;
-	case AGREEMENT_TYPE_DEMAND_GOLD: strcpy (astr, "AGREEMENT_TYPE_DEMAND_GOLD");break;
-	case AGREEMENT_TYPE_DEMAND_STOP_TRADE: strcpy (astr, "AGREEMENT_TYPE_DEMAND_STOP_TRADE");break;
-	case AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS: strcpy (astr, "AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS"); break;
-    case AGREEMENT_TYPE_REDUCE_POLLUTION: strcpy (astr, "AGREEMENT_TYPE_REDUCE_POLLUTION"); break;
-	case AGREEMENT_TYPE_CEASE_FIRE: strcpy (astr, "AGREEMENT_TYPE_CEASE_FIRE");break;
-    case AGREEMENT_TYPE_PACT_CAPTURE_CITY: strcpy (astr, "AGREEMENT_TYPE_PACT_CAPTURE_CITY");break;
-    case AGREEMENT_TYPE_PACT_END_POLLUTION: strcpy (astr, "AGREEMENT_TYPE_PACT_END_POLLUTION");break;
-    case AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY: strcpy (astr, "AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY");break;
-	case AGREEMENT_TYPE_OFFER_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_OFFER_ADVANCE");break;
-	case AGREEMENT_TYPE_OFFER_CITY: strcpy (astr, "AGREEMENT_TYPE_OFFER_CITY");break;
-	case AGREEMENT_TYPE_OFFER_MAP: strcpy (astr, "AGREEMENT_TYPE_OFFER_MAP");break;
-	case AGREEMENT_TYPE_OFFER_GOLD: strcpy (astr, "AGREEMENT_TYPE_OFFER_GOLD");break;
-	case AGREEMENT_TYPE_EXCHANGE_ADVANCE: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_ADVANCE");break;
-	case AGREEMENT_TYPE_EXCHANGE_CITY: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_CITY");break;
-	case AGREEMENT_TYPE_EXCHANGE_MAP: strcpy (astr, "AGREEMENT_TYPE_EXCHANGE_MAP");break;
-	case AGREEMENT_TYPE_NO_PIRACY: strcpy (astr, "AGREEMENT_TYPE_NO_PIRACY"); break;
+    case AGREEMENT_TYPE_DEMAND_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_ADVANCE", sizeof(astr)); break;
+	case AGREEMENT_TYPE_DEMAND_CITY: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_MAP: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_GOLD: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_GOLD", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_STOP_TRADE: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_STOP_TRADE", sizeof(astr));break;
+	case AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_LEAVE_OUR_LANDS", sizeof(astr)); break;
+    case AGREEMENT_TYPE_REDUCE_POLLUTION: strlcpy(astr, "AGREEMENT_TYPE_REDUCE_POLLUTION", sizeof(astr)); break;
+	case AGREEMENT_TYPE_CEASE_FIRE: strlcpy(astr, "AGREEMENT_TYPE_CEASE_FIRE", sizeof(astr));break;
+    case AGREEMENT_TYPE_PACT_CAPTURE_CITY: strlcpy(astr, "AGREEMENT_TYPE_PACT_CAPTURE_CITY", sizeof(astr));break;
+    case AGREEMENT_TYPE_PACT_END_POLLUTION: strlcpy(astr, "AGREEMENT_TYPE_PACT_END_POLLUTION", sizeof(astr));break;
+    case AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY: strlcpy(astr, "AGREEMENT_TYPE_DEMAND_ATTACK_ENEMY", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_OFFER_ADVANCE", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_CITY: strlcpy(astr, "AGREEMENT_TYPE_OFFER_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_MAP: strlcpy(astr, "AGREEMENT_TYPE_OFFER_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_OFFER_GOLD: strlcpy(astr, "AGREEMENT_TYPE_OFFER_GOLD", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_ADVANCE: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_ADVANCE", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_CITY: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_CITY", sizeof(astr));break;
+	case AGREEMENT_TYPE_EXCHANGE_MAP: strlcpy(astr, "AGREEMENT_TYPE_EXCHANGE_MAP", sizeof(astr));break;
+	case AGREEMENT_TYPE_NO_PIRACY: strlcpy(astr, "AGREEMENT_TYPE_NO_PIRACY", sizeof(astr)); break;
     default :
         {
         sint32 unknown_agreement_type=0;
@@ -323,26 +323,47 @@ void Diplomacy_Log::LogBrokenAgreement(const sint32 owner, const sint32 recipien
 void Diplomacy_Log::Request2String(REQUEST_TYPE request, char astr[80])
 {
     switch (request) {
+    // TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
     case REQUEST_TYPE_GREETING: strcpy (astr, "REQUEST_TYPE_GREETING"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_ADVANCE: strcpy (astr, "REQUEST_TYPE_DEMAND_ADVANCE"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_CITY: strcpy (astr, "REQUEST_TYPE_DEMAND_CITY"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_MAP: strcpy (astr, "REQUEST_TYPE_DEMAND_MAP"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_GOLD: strcpy (astr, "REQUEST_TYPE_DEMAND_GOLD"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_STOP_TRADE: strcpy (astr, "REQUEST_TYPE_DEMAND_STOP_TRADE"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_ATTACK_ENEMY: strcpy (astr, "REQUEST_TYPE_DEMAND_ATTACK_ENEMY"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_LEAVE_OUR_LANDS: strcpy (astr, "REQUEST_TYPE_DEMAND_LEAVE_OUR_LANDS"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_DEMAND_REDUCE_POLLUTION: strcpy (astr, "REQUEST_TYPE_DEMAND_REDUCE_POLLUTION"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_ADVANCE: strcpy (astr, "REQUEST_TYPE_OFFER_ADVANCE"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_CITY: strcpy (astr, "REQUEST_TYPE_OFFER_CITY"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_MAP: strcpy (astr, "REQUEST_TYPE_OFFER_MAP"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_GOLD: strcpy (astr, "REQUEST_TYPE_OFFER_GOLD"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_CEASE_FIRE: strcpy (astr, "REQUEST_TYPE_OFFER_CEASE_FIRE"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_PERMANENT_ALLIANCE: strcpy (astr, "REQUEST_TYPE_OFFER_PERMANENT_ALLIANCE"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_OFFER_PACT_CAPTURE_CITY: strcpy (astr, "REQUEST_TYPE_OFFER_PACT_CAPTURE_CITY"); break;
+    // TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
     case REQUEST_TYPE_OFFER_PACT_END_POLLUTION: strcpy (astr, "REQUEST_TYPE_OFFER_PACT_END_POLLUTION"); break;
+	// TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
 	case REQUEST_TYPE_EXCHANGE_ADVANCE: strcpy (astr, "REQUEST_TYPE_EXCHANGE_ADVANCE"); break;
+    // TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
     case REQUEST_TYPE_EXCHANGE_CITY: strcpy (astr, "REQUEST_TYPE_EXCHANGE_CITY"); break;
+    // TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
     case REQUEST_TYPE_EXCHANGE_MAP: strcpy (astr, "REQUEST_TYPE_EXCHANGE_MAP"); break;
+    // TODO(phase-2): strcpy → strlcpy — dst is `char *`, capacity unknown at call site
     case REQUEST_TYPE_DEMAND_NO_PIRACY: strcpy (astr, "REQUEST_TYPE_DEMAND_NO_PIRACY"); break;
     default:
         {
@@ -377,11 +398,11 @@ void Diplomacy_Log::LogTone(const sint32 sender, const sint32 reciever, const AT
     char astr[80];
 
     switch (attitude) {
-    case ATTITUDE_TYPE_STRONG_HOSTILE: strcpy (astr, "ATTITUDE_TYPE_STRONG_HOSTILE"); break;
-	case ATTITUDE_TYPE_WEAK_HOSTILE:  strcpy (astr, "ATTITUDE_TYPE_WEAK_HOSTILE"); break;
-	case ATTITUDE_TYPE_NEUTRAL:  strcpy (astr, "ATTITUDE_TYPE_NEUTRAL"); break;
-	case ATTITUDE_TYPE_WEAK_FRIENDLY:  strcpy (astr, "ATTITUDE_TYPE_WEAK_FRIENDLY"); break;
-	case ATTITUDE_TYPE_STRONG_FRIENDLY:  strcpy (astr, "ATTITUDE_TYPE_STRONG_FRIENDLY"); break;
+    case ATTITUDE_TYPE_STRONG_HOSTILE: strlcpy(astr, "ATTITUDE_TYPE_STRONG_HOSTILE", sizeof(astr)); break;
+	case ATTITUDE_TYPE_WEAK_HOSTILE:  strlcpy(astr, "ATTITUDE_TYPE_WEAK_HOSTILE", sizeof(astr)); break;
+	case ATTITUDE_TYPE_NEUTRAL:  strlcpy(astr, "ATTITUDE_TYPE_NEUTRAL", sizeof(astr)); break;
+	case ATTITUDE_TYPE_WEAK_FRIENDLY:  strlcpy(astr, "ATTITUDE_TYPE_WEAK_FRIENDLY", sizeof(astr)); break;
+	case ATTITUDE_TYPE_STRONG_FRIENDLY:  strlcpy(astr, "ATTITUDE_TYPE_STRONG_FRIENDLY", sizeof(astr)); break;
     default:
         {
             sint32 unknown_attitude=0;

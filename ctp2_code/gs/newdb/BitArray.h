5faee94cd25f8c5b0d5cf3e013b3f79a25e782d8
Aleksander Kwiatkowski <bobikx@poczta.fm>
2026-06-06 07:59:27 +0200

    refactor(smart-pointers): convert BitArray to unique_ptr
    Replace raw uint8* array with std::unique_ptr<uint8[]> for automatic
    memory management. Eliminates manual new[]/delete[] and null check.
    
    - Header: add <memory>, change m_array type
    - Ctor: use std::make_unique<uint8[]>(size)
    - Dtor: remove explicit cleanup (unique_ptr handles it)
    - Clear: add .get() for memset
    
    Builds clean, fast+unit tests pass.

--- ctp2_code/gs/newdb/BitArray.h
+++ ctp2_code/gs/newdb/BitArray.h
@@ -4,27 +4,24 @@
 #ifndef __BIT_ARRAY_H__
 #define __BIT_ARRAY_H__
 
+#include <memory>
+
 #define k_BITS_PER_ENTRY 8
 class BitArray
 {
 private:
-	uint8 *m_array;
+	std::unique_ptr<uint8[]> m_array;
 	sint32 m_numBits;
 
 public:
 	BitArray(sint32 numBits) {
 		m_numBits = numBits;
-		m_array = new uint8[(m_numBits + (k_BITS_PER_ENTRY - 1))/k_BITS_PER_ENTRY];
+		m_array = std::make_unique<uint8[]>((m_numBits + (k_BITS_PER_ENTRY - 1))/k_BITS_PER_ENTRY);
 	}
-	~BitArray() {
-		if(m_array) {
-			delete [] m_array;
-			m_array = NULL;
-		}
-	}
+	~BitArray() = default;
 
 	void Clear() {
-		memset(m_array, 0, (m_numBits + (k_BITS_PER_ENTRY - 1)) / k_BITS_PER_ENTRY);
+		memset(m_array.get(), 0, (m_numBits + (k_BITS_PER_ENTRY - 1)) / k_BITS_PER_ENTRY);
 	}
 
 	void SetBit(sint32 bit) {
